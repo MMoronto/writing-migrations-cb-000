2c700e42acea818a7ed7a1041588d3b79f4565b6
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-02-04 16:55:27 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,9 +2,4 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :grade, :birthdate, :integer, :string
   end
-  
-  def change
-    add_column :birthdate, :string
-  end
-  
 end
